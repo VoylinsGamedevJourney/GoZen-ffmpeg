c10f6e1b8c6afd1f3bb9cfd9e981cf64a63a4215
voylin <0voylin0@gmail.com>
2023-11-12 13:54:40 +0900

    Crashes editor

--- SConstruct
+++ SConstruct
@@ -1,12 +1,13 @@
 import platform
 
 env = SConscript("godot-cpp/SConstruct")
-env.Append(CPPPATH="src/")
+env.Append(CPPPATH=["/usr/include/gstreamer-1.0", "/usr/include/glib-2.0", "/usr/lib/glib-2.0/include", "/usr/include/sysprof-6", "src/"])
 
 # Determine the platform and set the appropriate link flags
 if platform.system() == "Linux": # Linux
   print("Linux system detected")
   env.Append(LIBS=["avcodec", "avformat", "avfilter", "avdevice", "avutil", "swscale", "swresample"])
+  env.Append(LIBS=["gstreamer-1.0", "gobject-2.0", "glib-2.0"])
 elif platform.system() == "Windows": # Windows - probably not working
   print("Windows system detected")
   env.Append(LIBS=["avcodec", "avformat", "avfilter", "avdevice", "avutil", "swscale", "swresample"])
--- src/gozen_importer.cpp
+++ src/gozen_importer.cpp
@@ -23,76 +23,76 @@ int GoZenImporter::get_video_streams(String file_path) {
     UtilityFunctions::printerr("Could not open source file!");
     return -1;
   }
-//
-//  // Retrieve stream info
-//  if (avformat_find_stream_info(p_format_context, NULL) < 0) {
-//    UtilityFunctions::printerr("No stream info in source file!");
-//    return -1;
-//  }
-//
-//  if (open_codec_context(&video_stream_idx, &p_video_codec_context, p_format_context, AVMEDIA_TYPE_VIDEO) >= 0) {
-//    p_video_stream = p_format_context->streams[video_stream_idx];
-//
-//    // Allocate image where decoded image will be put
-//    width = p_video_codec_context->width;
-//    height = p_video_codec_context->height;
-//    pix_fmt = p_video_codec_context->pix_fmt;
-//    ret = av_image_alloc(p_video_dst_data, video_dst_linesize, width, height, pix_fmt, 1);
-//    if (ret < 0) {
-//      UtilityFunctions::printerr("Could not allocate raw video buffer!");
-//      goto end;
-//    }
-//    video_dst_bufsize = ret;
-//  }
-//  if (open_codec_context(&audio_stream_idx, &p_audio_codec_context, p_format_context, AVMEDIA_TYPE_AUDIO) >= 0) {
-//    p_audio_stream = p_format_context->streams[audio_stream_idx];
-//  }
-//
-//  // Dumb input info in stderr 
-//  av_dump_format(p_format_context, 0, file_path.utf8(), 0);
-//
-//  if (!p_audio_stream && !p_video_stream) {
-//    UtilityFunctions::printerr("Could not find audio or video stream in the input!");
-//    ret = 1;
-//    goto end;
-//  }
-// 
-//  p_frame = av_frame_alloc();
-//  if (!p_frame) {
-//    UtilityFunctions::printerr("Could not allocate frame\n");
-//    ret = AVERROR(ENOMEM);
-//    goto end;
-//  }
-// 
-//  p_packet = av_packet_alloc();
-//  if (!p_packet) {
-//    UtilityFunctions::printerr("Could not allocate packet!");
-//    ret = AVERROR(ENOMEM);
-//    goto end;
-//  }
-//
-//  if (p_video_stream)
-//    UtilityFunctions::print("Demuxing video from file");
-//  if (p_audio_stream)
-//    UtilityFunctions::print("Demuxing audio from file");
-//
-//  // Read frames from the file
-//  while (av_read_frame(p_format_context, p_packet) >= 0) {
-//    // Check if packet belongs to a stream we need
-//    if (p_packet->stream_index == video_stream_idx)
-//      ret = decode_packet(p_video_codec_context, p_packet, p_frame, width, height, pix_fmt);
-//    else if (p_packet->stream_index == audio_stream_idx)
-//      ret = decode_packet(p_audio_codec_context, p_packet, p_frame, width, height, pix_fmt);
-//    av_packet_unref(p_packet);
-//    if (ret < 0)
-//      break;
-//  }
-//
-//  // Flush the decoders
-//  if (p_video_codec_context)
-//    decode_packet(p_video_codec_context, NULL, p_frame, width, height, pix_fmt);
-//  if (p_audio_codec_context)
-//    decode_packet(p_audio_codec_context, NULL, p_frame, width, height, pix_fmt);
+
+  // Retrieve stream info
+  if (avformat_find_stream_info(p_format_context, NULL) < 0) {
+    UtilityFunctions::printerr("No stream info in source file!");
+    return -1;
+  }
+
+  if (open_codec_context(&video_stream_idx, &p_video_codec_context, p_format_context, AVMEDIA_TYPE_VIDEO) >= 0) {
+    p_video_stream = p_format_context->streams[video_stream_idx];
+
+    // Allocate image where decoded image will be put
+    width = p_video_codec_context->width;
+    height = p_video_codec_context->height;
+    pix_fmt = p_video_codec_context->pix_fmt;
+    ret = av_image_alloc(p_video_dst_data, video_dst_linesize, width, height, pix_fmt, 1);
+    if (ret < 0) {
+      UtilityFunctions::printerr("Could not allocate raw video buffer!");
+      goto end;
+    }
+    video_dst_bufsize = ret;
+  }
+  if (open_codec_context(&audio_stream_idx, &p_audio_codec_context, p_format_context, AVMEDIA_TYPE_AUDIO) >= 0) {
+    p_audio_stream = p_format_context->streams[audio_stream_idx];
+  }
+
+  // Dumb input info in stderr 
+  av_dump_format(p_format_context, 0, file_path.utf8(), 0);
+
+  if (!p_audio_stream && !p_video_stream) {
+    UtilityFunctions::printerr("Could not find audio or video stream in the input!");
+    ret = 1;
+    goto end;
+  }
+ 
+  p_frame = av_frame_alloc();
+  if (!p_frame) {
+    UtilityFunctions::printerr("Could not allocate frame\n");
+    ret = AVERROR(ENOMEM);
+    goto end;
+  }
+ 
+  p_packet = av_packet_alloc();
+  if (!p_packet) {
+    UtilityFunctions::printerr("Could not allocate packet!");
+    ret = AVERROR(ENOMEM);
+    goto end;
+  }
+
+  if (p_video_stream)
+    UtilityFunctions::print("Demuxing video from file");
+  if (p_audio_stream)
+    UtilityFunctions::print("Demuxing audio from file");
+
+  // Read frames from the file
+  while (av_read_frame(p_format_context, p_packet) >= 0) {
+    // Check if packet belongs to a stream we need
+    if (p_packet->stream_index == video_stream_idx)
+      ret = decode_packet(p_video_codec_context, p_packet, p_frame, width, height, pix_fmt);
+    else if (p_packet->stream_index == audio_stream_idx)
+      ret = decode_packet(p_audio_codec_context, p_packet, p_frame, width, height, pix_fmt);
+    av_packet_unref(p_packet);
+    if (ret < 0)
+      break;
+  }
+
+  // Flush the decoders
+  if (p_video_codec_context)
+    decode_packet(p_video_codec_context, NULL, p_frame, width, height, pix_fmt);
+  if (p_audio_codec_context)
+    decode_packet(p_audio_codec_context, NULL, p_frame, width, height, pix_fmt);
   UtilityFunctions::print("Demuxing succeeded!");
 
 end:
